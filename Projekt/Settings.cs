310fd7c7a2c96167d4c9f4a8d3b0a4b8d2b782e5
z0lt4np4l1nk4s <65424998+z0lt4np4l1nk4s@users.noreply.github.com>
2020-05-24 15:43:54 +0200

    Add files via upload

--- Projekt/Settings.cs
+++ Projekt/Settings.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Xml.Serialization;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Projekt
 {
-    class Settings
+    public class Settings
     {
         public string fName { get; set; }
         public int numb { get; set; }
@@ -66,6 +72,44 @@ namespace Projekt
             sw.WriteLine(sIndex);
             sw.WriteLine(description);
             sw.Close();
+        }
+
+        public void SetJSONSettings(Settings settings)
+        {
+            string json = JsonConvert.SerializeObject(settings);
+            File.WriteAllText("settings.json", json);
+        }
+
+        public void GetJSONSettings()
+        {
+            JObject jObject = JObject.Parse(File.ReadAllText("settings.json"));
+            fName = (string)jObject["fName"];
+            numb = (int)jObject["numb"];
+            path = (string)jObject["path"];
+            check = (bool)jObject["check"];
+            seconds = (int)jObject["seconds"];
+            sIndex = (int)jObject["sIndex"];
+            description = (bool)jObject["description"];
+        }
+
+        public void SetXMLSettings(Settings settings)
+        {
+            XmlSerializer writer = new XmlSerializer(typeof(Settings));
+            FileStream file = File.Create("settings.xml");
+            writer.Serialize(file, settings);
+            file.Close();
+        }
+
+        public void GetXMLSettings()
+        {
+            XElement element = XElement.Load("settings.xml");
+            fName = element.Element("fName").Value;
+            numb = int.Parse(element.Element("numb").Value);
+            path = element.Element("path").Value;
+            check = bool.Parse(element.Element("check").Value);
+            seconds = int.Parse(element.Element("seconds").Value);
+            sIndex = int.Parse(element.Element("sIndex").Value);
+            description = bool.Parse(element.Element("description").Value);
         }
     }
 }
