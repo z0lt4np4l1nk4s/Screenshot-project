2c7fd27bdbe343cc9140f5acacb4514eb22567b2
z0lt4np4l1nk4s <65424998+z0lt4np4l1nk4s@users.noreply.github.com>
2020-05-24 15:29:49 +0200

    Add files via upload

--- Projekt/ImageList.cs
+++ Projekt/ImageList.cs
@@ -10,20 +10,22 @@ namespace Projekt
     class ImageList
     {
         public static string SelectedImageName;
+        private List<string> _img = new List<string>();
         public List<string> imgList
         {
             get
             {
+                
                 Settings settings = new Settings();
                 settings.GetSettings();
                 DirectoryInfo di = new DirectoryInfo(settings.path);
                 foreach (FileInfo fi in di.GetFiles())
                 {
-                    if (fi.Name.Contains(".jpg")) imgList.Add(fi.Name);
-                    if (fi.Name.Contains(".bmp")) imgList.Add(fi.Name);
-                    if (fi.Name.Contains(".png")) imgList.Add(fi.Name);
+                    if (fi.Name.Contains(".jpg")) _img.Add(fi.Name);
+                    if (fi.Name.Contains(".bmp")) _img.Add(fi.Name);
+                    if (fi.Name.Contains(".png")) _img.Add(fi.Name);
                 }
-                return this.imgList;
+                return this._img;
             }
         }
     }
